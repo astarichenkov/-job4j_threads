6e5023aae37fe7825110456dd0d8dc075f8897f7
Anton.Starichenkov <anton.starichenkov@gmail.com>
2021-09-25 23:02:06 +0300

    4. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/FileDownload.java
+++ src/main/java/ru/job4j/concurrent/FileDownload.java
@@ -8,21 +8,32 @@ import java.net.URL;
 public class FileDownload implements Runnable {
     private final String url;
     private final int speed;
+    private final String file;
 
-    public FileDownload(String url, int speed) {
+    public FileDownload(String url, int speed, String file) {
         this.url = url;
         this.speed = speed;
+        this.file = file;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(file)) {
             byte[] dataBuffer = new byte[speed];
             int bytesRead;
+            long readTime;
+            long stop;
+            long start = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
+                stop = System.currentTimeMillis();
+                readTime = stop - start;
+                System.out.println(readTime);
+                if (readTime < 1000) {
+                    Thread.sleep(1000 - readTime);
+                }
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                Thread.sleep(1000);
+                start = System.currentTimeMillis();
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -30,9 +41,18 @@ public class FileDownload implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Usage: "
+                    + "java -jar FileDownload.jar "
+                    + "URL "
+                    + "DOWNLOAD_SPEED (bytes per second) "
+                    + "DEST_FILE");
+        }
+
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new FileDownload(url, speed));
+        String file = args[2];
+        Thread wget = new Thread(new FileDownload(url, speed, file));
         wget.start();
         wget.join();
     }
