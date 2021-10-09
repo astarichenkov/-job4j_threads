801d00b565dd3724ef76b2d61c126f486861ffb4
Anton.Starichenkov <anton.starichenkov@gmail.com>
2021-10-10 00:46:36 +0300

    2. Обеспечить остановку потребителя. [#66825]

--- src/main/java/ru/job4j/buffer/ParallelSearch.java
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -24,10 +24,6 @@ public class ParallelSearch {
                     for (int index = 0; index != 3; index++) {
                         try {
                             queue.offer(index);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        try {
                             Thread.sleep(500);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
