9e10c80ac5424fa8d997802c03f974231485383b
Anton.Starichenkov <anton.starichenkov@gmail.com>
2021-10-14 00:45:35 +0300

    0. CAS - операции [#6859 #214131]

--- src/test/java/ru/job4j/cache/CASCountTest.java
+++ src/test/java/ru/job4j/cache/CASCountTest.java
@@ -1,8 +1,8 @@
 package ru.job4j.cache;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CASCountTest {
 
@@ -21,5 +21,27 @@ public class CASCountTest {
         casCount.increment();
         casCount.get();
         assertEquals(1, casCount.get());
+    }
+
+    @Test
+    public void whenExecute2Thread() throws InterruptedException {
+        final CASCount count = new CASCount();
+        Thread first = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                count.increment();
+            }
+        });
+
+        Thread second = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                count.increment();
+            }
+        });
+
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        assertEquals(count.get(), 10);
     }
 }
